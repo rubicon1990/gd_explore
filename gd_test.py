a01f3ce8aa8e05b3e6738dedadd90e5a331c1459
rubicon1990 <wei.yuxin@hotmail.com>
2020-10-07 17:20:58 +0800

    Update gd_test.py

--- gd_test.py
+++ gd_test.py
@@ -70,27 +70,28 @@ def plotting(history_loss, triple_loss_func):
     plt.legend(loc='best')
 
 
-x_train = np.arange(10000)
-a = tf.constant(30.0)
-learning_rate = 0.0001
-n_iter = 100
-
-print(0, a.numpy(), loss_calulate(x_train, a).numpy())
-hist_loss = {'l': [loss_calulate(x_train, a).numpy()],
-             'a': [a.numpy()]}
-
-for i in range(1, n_iter + 1):
-    a = train_step(x_train, a, learning_rate)
-    loss = loss_calulate(x_train, a).numpy()
-    print(i, a.numpy(), loss)
-    hist_loss['l'].append(loss)
-    hist_loss['a'].append(a)
-
-a_min = argmin(x_train)
-a_s = np.linspace(a_min - 1, a_min + 1, 100)
-l_s = (lambda _a: loss_calulate(x_train, _a))(a_s)
-
-plt.figure(figsize=(15, 5))
-plotting(hist_loss, (a_min, a_s, l_s))
-plt.tight_layout()
-plt.show()
+if __name__ == "__main__":
+    x_train = np.arange(10000)
+    a = tf.constant(30.0)
+    learning_rate = 0.0001
+    n_iter = 100
+    
+    print(0, a.numpy(), loss_calulate(x_train, a).numpy())
+    hist_loss = {'l': [loss_calulate(x_train, a).numpy()],
+                 'a': [a.numpy()]}
+    
+    for i in range(1, n_iter + 1):
+        a = train_step(x_train, a, learning_rate)
+        loss = loss_calulate(x_train, a).numpy()
+        print(i, a.numpy(), loss)
+        hist_loss['l'].append(loss)
+        hist_loss['a'].append(a)
+    
+    a_min = argmin(x_train)
+    a_s = np.linspace(a_min - 1, a_min + 1, 100)
+    l_s = (lambda _a: loss_calulate(x_train, _a))(a_s)
+    
+    plt.figure(figsize=(15, 5))
+    plotting(hist_loss, (a_min, a_s, l_s))
+    plt.tight_layout()
+    plt.show()
